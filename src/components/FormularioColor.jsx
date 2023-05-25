d06b727c885c4a864db50d657cee08644a6ebf47
Nicolas Herrera <herreranicoo17@gmail.com>
2023-05-25 16:35:32 -0300

    Agregue la logica para evitar que se recargue la pagina en el evento submit, y tambien agregue la logica para que al momento de que el usuario elija un color el mismo se muestre en el contenedor color utilizando el evento onchange en el input

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import ContenedorColor from "./ContenedorColor";
 
 const FormularioColor = () => {
+  const [color, setColor] = useState("#000000");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <Form className="border border-3 shadow p-3 text-center">
+    <Form
+      className="border border-3 shadow p-3 text-center"
+      onSubmit={handleSubmit}
+    >
       <Form.Group className="mb-3">
         <Form.Label className="mx-3">Elegir color</Form.Label>
-        <Form.Control type="color" className="d-inline-block" />
+        <Form.Control
+          type="color"
+          className="d-inline-block"
+          onChange={(e) => setColor(e.target.value)}
+        />
       </Form.Group>
-      <ContenedorColor color={"yellow"}></ContenedorColor>
+      <ContenedorColor color={color}></ContenedorColor>
       <Button variant="primary" type="submit">
         Guardar color
       </Button>
