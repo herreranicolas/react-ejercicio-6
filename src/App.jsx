b718729f207b9f50603941bc6ebbd6f3fd300675
Nicolas Herrera <herreranicoo17@gmail.com>
2023-05-25 15:50:16 -0300

    Agregue el maquetado inicial de la seccion colores donde se mostraran los colores guardados

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ import FormularioColor from "./components/FormularioColor";
 
 function App() {
   return (
-    <Container>
-      <h1 className="text-center display-3">Administrador de Colores</h1>
-      <FormularioColor></FormularioColor>
-    </Container>
+    <>
+      <h1 className="text-center display-3 my-3">Administrador de Colores</h1>
+      <Container>
+        <FormularioColor></FormularioColor>
+      </Container>
+      <Container>
+        <h2 className="display-6 my-3">Lista de Colores</h2>
+        <p className="lead text-center">No hay colores guardados</p>
+      </Container>
+    </>
   );
 }
 
